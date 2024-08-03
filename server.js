e1ddd82ba73d15eb49fcb19e2ffc9ed387684fa8
Pranav <pranav.2002tis@gmail.com>
2024-08-03 23:23:28 +0530

    Made modifications

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 8080;
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 
 // Database
-mongoose.connect(
-  `mongodb+srv://admin:pranav.2002@trial.bc1fvq7.mongodb.net/Unipan_Backend`,
-  {
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => {
   console.error(error);
